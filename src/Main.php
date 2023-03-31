1d55fc5d0607d558e09039c22dcd9257abf6dede
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-03-31 18:38:17 +1000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,3 +1,30 @@
-namespace skyss0fly\FWEssents
+<?php
+namespace skyss0fly\FWEssents;
 
 use skyss0fly\src\config\config;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase
+{
+  private CustomConfig $config;
+	private static self $instance;
+
+	protected function onLoad(): void
+			
+	
+	{
+		self::$instance = $this;
+		$this->saveResource("config.yml");
+		$this->config = new CustomConfig(new Config($this->getDataFolder() . "config.yml", Config::YAML));
+    $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "FWEssents: V0.0.0.1 Loaded successfully");
+	}
+
+  }
+
+	protected function onEnable(): void
+			 $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "FWEssents: V0.0.0.1 Enabled successfully! Have a great day!");
+}
+		
+	
